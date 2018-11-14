c5176cfa50e6611bd32836168f47314f70917a05
Tim S <stahta01@users.sourceforge.net>
2018-11-14 17:55:06 -0500

    updraded asx to V5.10

---
+++ as-5.1.0/config.h
@@ -0,0 +1,18 @@
+#define VERSION    "V05.10 (GCC6809)" /* Version number */
+#define COPYRIGHT  "2014"             /* Copyright year */
+#define NCPS       512                /* Characters per symbol */
+#define NINPUT     (NCPS*2)           /* Input buffer size */
+#define FILSPC     (NINPUT+10)        /* File spec length */
+#define MAXHASHBIT 16                 /* Maximum bits for a hash table */
+#define NOFORMFEED 1                  /* Disable form feed in listing */
+
+/*
+ * To include NoICE Debugging set non-zero
+ */
+#define NOICE      1
+
+/*
+ * To include SDCC Debugging set non-zero
+ */
+#define SDCDB      1
+
